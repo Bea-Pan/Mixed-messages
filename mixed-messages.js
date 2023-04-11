60b1f13739f510d8945d27d7f0f69af12ec5dab3
Bea Pan <bpankowska@gmail.com>
2023-04-11 19:39:42 +0200

    second part of the message - quote

--- mixed-messages.js
+++ mixed-messages.js
@@ -20,3 +20,29 @@ function lifeSphere(){
 }
 
 console.log(lifeSphere());
+
+//Second part of the message - Inspirational Quote
+
+function inspirationalQuote(){
+    let quote = Math.floor(Math.random()*5)
+
+    switch(quote){
+        case 0:
+            return '"You miss 100% of the shots you don\'t take"-Wayne Gretzky';
+            break;
+        case 1: 
+            return '"Do one thing everyday that scares you"-Eleanor Roosevelt';
+            break;
+        case 2:
+            return '"If you have everything under control, you\'re not moving fast enough"-Mario Andretti';
+            break;
+        case 3:
+            return '"It does not matter how slowly you go as long as you don\'t stop"-Confucius';
+            break;
+        case 4:
+            return '"Ever tried. Ever failed. No matter. Try again. Fail again. Fail better"-Samuel Beckett';
+            break;
+    }
+
+}
+console.log(inspirationalQuote())
